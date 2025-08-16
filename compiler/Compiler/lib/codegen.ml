66575c3a326dfe4ccbd189c75a2461527eb0e50a
Ukyo1121 <1755379549@qq.com>
2025-08-16 22:12:16 +0800

    修复语法错误

--- compiler/Compiler/lib/codegen.ml
+++ compiler/Compiler/lib/codegen.ml
@@ -529,7 +529,7 @@ let gen_function func =
         let save_regs_asm =
             let reg_saves = List.mapi (fun i reg ->
                 Printf.sprintf "    sw %s, %d(sp)" reg (i * 4)
-                ctx.saved_regs
+                ctx.saved_regs)
             in
             let ra_save = Printf.sprintf "    sw ra, %d(sp)" (List.length ctx.saved_regs * 4) in
             String.concat "\n" (reg_saves @ [ra_save])
